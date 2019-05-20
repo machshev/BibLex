b757566cdd83fe1efb2763f5fa4125ac4247e6d9
David James McCorrie <david@machshev.co.uk>
2019-05-20 23:38:34 +0100

    Basic project structure

---
+++ src/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='BibLex',
+      version='1.0',
+      description='Hebrew to Greek lexicon based on biblical quotes from the OT in the NT',
+      author='David McCorrie',
+      author_email='david@machshev.co.uk',
+      url='https://machshev.co.uk',
+      packages=['biblex'],
+     )
